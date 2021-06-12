3204c657699ab034d797f1605abac30e2b7467d6
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2021-06-12 13:23:13 +0100

    Create autoexec.sh

---
+++ rtklib/str2str/autoexec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# by: JOR
+# Date: 14MAY21
+# Function: autoexec for Linux
+# Leave this script in /home/pi
+# Script: autoexec.sh
+
+HOMEPATH="/home/pi"
+
+# Configure hardware serial port
+stty -F /dev/ttyS0 clocal raw speed 115200
+
+# Get RTCM3 from RTK2GO and send to Ardusimple on S0
+$HOMEPATH/rtklib/str2str/str2str -in ntripcli://:@rtk2go.com/Umricam -out serial://ttyS0:115200:8:n:1
